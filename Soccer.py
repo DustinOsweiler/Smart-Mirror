44d4acfee7883698019311cda1ef2b8c532b57e9
Dustin Osweiler <dosweiler@carthage.edu>
2022-03-07 19:49:09 -0600

    Got rid of the key

--- Soccer.py
+++ Soccer.py
@@ -4,7 +4,7 @@ conn = http.client.HTTPSConnection("api-football-v1.p.rapidapi.com")
 
 headers = {
     'x-rapidapi-host': "api-football-v1.p.rapidapi.com",
-    'x-rapidapi-key': "0023c6c134mshfe6d1aea7cba72fp156c83jsn71912b0b2f43"
+    'x-rapidapi-key': ""
     }
 
 conn.request("GET", "/v3/fixtures?season=2021&team=33", headers=headers)
