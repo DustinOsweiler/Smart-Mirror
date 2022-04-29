6ffbd3d411d66d3ab95de70ab9b05b7eeeb9ea00
Dustin Osweiler <dosweiler@carthage.edu>
2022-04-28 22:39:47 -0500

    Delete Main.ts

--- Main.ts
+++
@@ -1,8 +0,0 @@
-let cvs = <HTMLCanvasElement> document.querySelector('Mirror');
-let c = cvs.getContext('2d');
-
-cvs.width = window.innerWidth;
-cvs.height = window.innerHeight;
-
-c.fillStyle='black';
-c.fillRect(0,0,cvs.width,cvs.height);
